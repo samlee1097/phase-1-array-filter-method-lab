6d3f604aeab6cf724a738f1dcbf6e43cb3ae039e
Samuel <samlee1097@yahoo.com>
2021-09-01 13:48:02 -0400

    commit tree 2a840791d6691a22f8bae20f0bb352a6bf5a2cfd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+function findMatching(array, name){
+    return array.filter(driversName => driversName.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(driversName => driversName.slice(0,name.length) == name)
+}
+
+function matchName(array, name){
+    return array.filter(driversName => driversName.name === name);
+}
